1040324a266ee8247ac309371913a56015780e87
Jon Turney <jon.turney@dronecode.org.uk>
2017-10-24 14:03:31 +0100

    Do breakdown by setup version, OS version and OS bitness

--- httpd-log-setup-analysis.py
+++ httpd-log-setup-analysis.py
@@ -9,9 +9,28 @@ class Agent():
         self.status = {}
         self.total = 0
 
+def breakdown(data, title):
+    print()
+    print('%-16s | count ' % (title))
+    print('-' * 33)
+    t = 0
+
+    for i in data:
+        t += data[i]
+
+    for i in sorted(data.keys(), key=lambda k: data[k], reverse=True):
+        print('%-16s | %6d (%2d%%) | ' % (i, data[i], 100*data[i]/t))
+    print('-' * 33)
+
 data = {}
 
+setup_versions = {}
+setup_oses = {}
+setup_bitnesses = {}
+
 r = re.compile(r'(\S*) (\S*) (\S*) (\[.*\]) \"GET /mirrors.lst .*\" (\S*) (\S*) \"(.*)\" "(.*)"')
+rc = re.compile(r'Cygwin-Setup/(\S*)(?: \(Windows NT (\S*);(\S*)\)|$)')
+
 for l in sys.stdin:
     m = re.match(r, l)
     if m:
@@ -29,6 +48,8 @@ for l in sys.stdin:
         if referer != "-":
             continue
 
+        original_agent = agent
+
         agent = re.sub(r' appid: [^)]*', '', agent)
         agent = re.sub(r'User-Agent: (.*)', r'\1', agent)
         agent = re.sub(r'.* \(compatible.*; (.*[Bb]ot/.*)\)', r'\1', agent)
@@ -37,12 +58,26 @@ for l in sys.stdin:
         agent = re.sub(r'.* (Firefox/\S*)', r'Firefox/(various)', agent)
         agent = re.sub(r'Python-urllib/\S*', r'Python-urllib/(various)', agent)
         agent = re.sub(r'.* WindowsPowerShell/(\S*)', r'WindowsPowerShell/\1', agent)
+        agent = re.sub(r'Cygwin-Setup/(\S*) .*', r'Cygwin-Setup/\1', agent)
 
         if agent not in data:
             data[agent] = Agent()
 
         data[agent].status[status] = data[agent].status.get(status, 0) + 1
         data[agent].ips.add(ip)
+
+        if original_agent.startswith('Cygwin'):
+            mc = re.match(rc, original_agent)
+            if mc:
+                ver = mc.group(1)
+                os = mc.group(2)
+                bitness = mc.group(3)
+
+                setup_versions[ver] = setup_versions.get(ver, 0) + 1
+                if os:
+                    setup_oses[os] = setup_oses.get(os, 0) + 1
+                if bitness:
+                    setup_bitnesses[bitness] = setup_bitnesses.get(bitness, 0) + 1
 
 grand_total = 0
 grand_total_ips = 0
@@ -85,3 +120,7 @@ print('excluded: non-empty referrer (browser hits), user-agents with only a sing
 print('-' * max_width)
 print('%-*s | %5d       | %-45s | %6d       |' % (max_agent_width, 'totals', grand_total_ips, '', grand_total))
 print('-' * max_width)
+
+breakdown(setup_versions, "setup version")
+breakdown(setup_oses, "OS version")
+breakdown(setup_bitnesses, "bitness")
