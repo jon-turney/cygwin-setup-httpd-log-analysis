7bc91f07d9d87a13dfb694cf84c6de5b98edd83a
Jon Turney <jon.turney@dronecode.org.uk>
2017-11-09 20:05:27 +0000

    Also analyse setup downloads

--- httpd-log-setup-analysis.py
+++ httpd-log-setup-analysis.py
@@ -46,8 +46,9 @@ data = {}
 setup_versions = {}
 setup_oses = {}
 setup_bitnesses = {}
+setup_downloads = {}
 
-r = re.compile(r'(\S*) (\S*) (\S*) (\[.*\]) \"GET /mirrors.lst .*\" (\S*) (\S*) \"(.*)\" "(.*)"')
+r = re.compile(r'(\S*) (\S*) (\S*) (\[.*\]) \"GET /(\S*) .*\" (\S*) (\S*) \"(.*)\" "(.*)"')
 rc = re.compile(r'Cygwin-Setup/(\S*)(?: \(Windows NT (\S*);(\S*)\)|$)')
 
 for l in sys.stdin:
@@ -57,49 +58,55 @@ for l in sys.stdin:
         identity = m.group(2)
         username = m.group(3)
         timestamp = m.group(4)
-        status = m.group(5)
-        size = m.group(6)
-        referer = m.group(7)
-        agent = m.group(8)
+        path = m.group(5)
+        status = m.group(6)
+        size = m.group(7)
+        referer = m.group(8)
+        agent = m.group(9)
 
-        # ignore lines which have a referer: these are browsers following a link
-        # to mirrors.lst
-        if referer != "-":
-            continue
+        if path == "mirrors.lst":
+            # ignore lines which have a referer: these are browsers following a link
+            # to mirrors.lst
+            if referer != "-":
+                continue
 
-        original_agent = agent
+            original_agent = agent
 
-        agent = re.sub(r' appid: [^)]*', '', agent)
-        agent = re.sub(r'User-Agent: (.*)', r'\1', agent)
-        agent = re.sub(r'.* \(compatible.*; (.*[Bb]ot/.*)\)', r'\1', agent)
-        agent = re.sub(r'.* (Edge/\S*)', r'Edge/(various)', agent)
-        agent = re.sub(r'.* (Safari/\S*).*', r'Safari/(various)', agent)
-        agent = re.sub(r'.* (Firefox/\S*)', r'Firefox/(various)', agent)
-        agent = re.sub(r'Python-urllib/\S*', r'Python-urllib/(various)', agent)
-        agent = re.sub(r'.* WindowsPowerShell/(\S*)', r'WindowsPowerShell/\1', agent)
-        agent = re.sub(r'Cygwin-Setup/(\S*) .*', r'Cygwin-Setup/\1', agent)
+            agent = re.sub(r' appid: [^)]*', '', agent)
+            agent = re.sub(r'User-Agent: (.*)', r'\1', agent)
+            agent = re.sub(r'.* \(compatible.*; (.*[Bb]ot/.*)\)', r'\1', agent)
+            agent = re.sub(r'.* (Edge/\S*)', r'Edge/(various)', agent)
+            agent = re.sub(r'.* (Safari/\S*).*', r'Safari/(various)', agent)
+            agent = re.sub(r'.* (Firefox/\S*)', r'Firefox/(various)', agent)
+            agent = re.sub(r'Python-urllib/\S*', r'Python-urllib/(various)', agent)
+            agent = re.sub(r'.* WindowsPowerShell/(\S*)', r'WindowsPowerShell/\1', agent)
+            agent = re.sub(r'Cygwin-Setup/(\S*) .*', r'Cygwin-Setup/\1', agent)
 
-        if agent not in data:
-            data[agent] = Agent()
+            if agent not in data:
+                data[agent] = Agent()
 
-        data[agent].status[status] = data[agent].status.get(status, 0) + 1
-        data[agent].ips.add(ip)
+            data[agent].status[status] = data[agent].status.get(status, 0) + 1
+            data[agent].ips.add(ip)
 
-        if original_agent.startswith('Cygwin'):
-            mc = re.match(rc, original_agent)
-            if mc:
-                ver = mc.group(1)
-                os = mc.group(2)
-                bitness = mc.group(3)
+            if original_agent.startswith('Cygwin'):
+                mc = re.match(rc, original_agent)
+                if mc:
+                    ver = mc.group(1)
+                    os = mc.group(2)
+                    bitness = mc.group(3)
 
-                OS.add(setup_versions, ver, ip)
-                if os:
-                    OS.add(setup_oses, os, ip)
-                if bitness:
-                    OS.add(setup_bitnesses, bitness, ip)
-            else:
-                ver = "Unknown (<=2.879)"
-                OS.add(setup_versions, ver, ip)
+                    OS.add(setup_versions, ver, ip)
+                    if os:
+                        OS.add(setup_oses, os, ip)
+                    if bitness:
+                        OS.add(setup_bitnesses, bitness, ip)
+                else:
+                    ver = "Unknown (<=2.879)"
+                    OS.add(setup_versions, ver, ip)
+        elif (path == 'setup-x86_64.exe') and (status == '200'):
+            OS.add(setup_downloads, 'x86_64', ip)
+        elif (path == 'setup-x86.exe') and (status == '200') and (ip != '125.174.164.21'):
+            OS.add(setup_downloads, 'x86', ip)
 
 grand_total = 0
 grand_total_ips = 0
@@ -152,3 +159,4 @@ print('-' * max_width)
 breakdown(setup_versions, "setup version")
 breakdown(setup_oses, "OS version")
 breakdown(setup_bitnesses, "bitness")
+breakdown(setup_downloads, "downloads")
